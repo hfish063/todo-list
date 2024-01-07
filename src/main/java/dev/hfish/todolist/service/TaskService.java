de099907081b0ea5976db8a94758c07933774ed8
haydenfish <hayden.fish@icloud.com>
2024-01-06 22:54:52 -0800

    Implemented additional TaskService methods

--- src/main/java/dev/hfish/todolist/service/TaskService.java
+++ src/main/java/dev/hfish/todolist/service/TaskService.java
@@ -6,5 +6,8 @@ import java.util.List;
 
 public interface TaskService {
     public List<Task> findAll();
+    public List<Task> findAllByCompletion();
+    public Task findById(Long theId);
     public Task save(Task theTask);
+    public void deleteById(Long theId);
 }
--- src/main/java/dev/hfish/todolist/service/TaskServiceImpl.java
+++ src/main/java/dev/hfish/todolist/service/TaskServiceImpl.java
@@ -5,7 +5,9 @@ import dev.hfish.todolist.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -27,8 +29,52 @@ public class TaskServiceImpl implements TaskService {
         return taskRepository.findAll();
     }
 
+    /**
+     * Returns list of all tasks where isComplete is TRUE (task is completed)
+     * usage: find every task that has been completed
+     *
+     * @return List of every Task element in db where completion status is TRUE
+     */
+    // TODO: fix ignorant method implementation (no need to query for every element)
+    @Override
+    public List<Task> findAllByCompletion() {
+        List<Task> taskList = taskRepository.findAll();
+        List<Task> completedTaskList = new ArrayList<>();
+
+        for(Task currentTask : taskList) {
+            if (currentTask.isComplete()) {
+                completedTaskList.add(currentTask);
+            }
+        }
+
+        return completedTaskList;
+    }
+
+    /**
+     * Finds Task stored in db with corresponding id, if result is not found
+     * throw RuntimeException
+     *
+     * @param theId id value (column) of task we are searching for in db
+     * @return Task obtained from db if available, else throw exception
+     */
+    @Override
+    public Task findById(Long theId) {
+        Optional<Task> result = taskRepository.findById(theId);
+
+        if (result.isEmpty()) {
+            throw new RuntimeException("Could not locate task with id - " + theId);
+        }
+
+        return result.get();
+    }
+
     @Override
     public Task save(Task theTask) {
         return taskRepository.save(theTask);
+    }
+
+    @Override
+    public void deleteById(Long theId) {
+        taskRepository.deleteById(theId);
     }
 }
