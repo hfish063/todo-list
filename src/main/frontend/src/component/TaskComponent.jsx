3b1cb9c223d68700da019fa40ebff1aaa25e1752
haydenfish <hayden.fish@icloud.com>
2024-01-07 17:18:08 -0800

    Stub values for Task creation form functionality

---
+++ src/main/frontend/src/component/TaskComponent.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class TaskComponent extends Component {
+    render() {
+        return (
+            <h1>New Task</h1>
+        )
+    }
+}
+
+export default TaskComponent
