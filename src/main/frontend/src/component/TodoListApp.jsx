0c81b290618e584bf08886cbd2cc8d07144386e3
haydenfish <hayden.fish@icloud.com>
2024-01-07 17:16:30 -0800

    Added routing functionality for central endpoints / and /home

--- src/main/frontend/src/component/TodoListApp.jsx
+++ src/main/frontend/src/component/TodoListApp.jsx
@@ -1,12 +1,29 @@
 import React, {Component} from "react";
 import ListTasksComponent from "./ListTasksComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 class TodoListApp extends Component {
     render() {
-        return ( <>
-            <h1>Todo-List Application</h1>
+        return ( 
+            // temp comment for debugging purposes:
+            // <>
+            //     <h1>To-do List Application</h1>
+            //     <hr></hr>
+            //     <ListTasksComponent/>
+            //     <hr></hr>
+            // </>
+
+            <>
+            <h1>To-Do List</h1>
             <hr></hr>
-            <ListTasksComponent/>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<ListTasksComponent />} />
+                    <Route path="/home" element={<ListTasksComponent />} />
+                </Routes>
+            </BrowserRouter>
+
             <hr></hr>
             </>
         )
